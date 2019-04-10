b5414528b486f9bb3bc157f017e0e66a901e6b64
liuzhouliang <525688067@qq.com>
2019-04-10 11:53:03 +0800

    q

---
+++ SVG/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * @ describe 
+ * 
+ * @author lzl
+ *
+ * @ time 2019/4/10 11:52
+ * 
+ * @ param
+ * 
+ * @ return
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
